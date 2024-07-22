f0c4a3a7feac8e29688f9753bab6ade8f6b98310
Vladimir <65397312+DunaevVK@users.noreply.github.com>
2024-07-22 18:38:22 +0700

    Add files via upload

---
+++ build/js/pages/index.js
@@ -0,0 +1,11 @@
+const someScriptsForCurrentPage = () => {
+    console.log('Здесь можно добавить какие-либо скрипты для текущей страницы');
+}
+
+window.addEventListener('load', () => {
+
+    // Здесь вызываем все скрипты для текущей страницы
+
+    someScriptsForCurrentPage()
+})
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJpbmRleC9zY3JpcHQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3Qgc29tZVNjcmlwdHNGb3JDdXJyZW50UGFnZSA9ICgpID0+IHtcclxuICAgIGNvbnNvbGUubG9nKCfQl9C00LXRgdGMINC80L7QttC90L4g0LTQvtCx0LDQstC40YLRjCDQutCw0LrQuNC1LdC70LjQsdC+INGB0LrRgNC40L/RgtGLINC00LvRjyDRgtC10LrRg9GJ0LXQuSDRgdGC0YDQsNC90LjRhtGLJyk7XHJcbn1cclxuXHJcbndpbmRvdy5hZGRFdmVudExpc3RlbmVyKCdsb2FkJywgKCkgPT4ge1xyXG5cclxuICAgIC8vINCX0LTQtdGB0Ywg0LLRi9C30YvQstCw0LXQvCDQstGB0LUg0YHQutGA0LjQv9GC0Ysg0LTQu9GPINGC0LXQutGD0YnQtdC5INGB0YLRgNCw0L3QuNGG0YtcclxuXHJcbiAgICBzb21lU2NyaXB0c0ZvckN1cnJlbnRQYWdlKClcclxufSkiXSwiZmlsZSI6ImluZGV4LmpzIn0=
